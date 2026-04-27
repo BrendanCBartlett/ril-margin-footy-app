24cb1452f71c2c262b74b826ea0fa21a53e6c05f
Brendan Bartlett <brendan.bartlett@rilaunceston.com.au>
2026-04-27 22:48:58 +1000

    Remove invalid supabase.restUrl debug log

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -6,5 +6,3 @@ export const supabase = createClient(
   "https://rgwknimuspoobbznskxi.supabase.co",
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
-
-console.log("✅ supabase REST URL:", supabase.restUrl);
