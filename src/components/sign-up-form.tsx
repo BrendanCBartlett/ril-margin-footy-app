be38304541a19854a8401d42833a46895c920fee
Brendan Bartlett <brendan.bartlett@rilaunceston.com.au>
2026-04-27 22:42:59 +1000

    Fix JSX closing tag in SignUpForm

--- src/components/sign-up-form.tsx
+++ src/components/sign-up-form.tsx
@@ -1,7 +1,12 @@
-'use client';
+"use client";
 
 export default function SignUpForm() {
   return (
     <div>
       <h2>Sign Up</h2>
-      <p>Sign‑up form will be implemented for the 2026 season.</p>
+      <p>
+        Sign-up form will be implemented for the 2026 season.
+      </p>
+    </div>
+  );
+}
