691ad9f8ba928320c321259c3b84afdd6aabaf69
Brendan Bartlett <brendan.bartlett@rilaunceston.com.au>
2026-04-28 11:47:35 +1000

    Fix LocaleProvider import path

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ThemeProvider } from "@dashboardpack/core/providers/theme-provider";
-import { LocaleProvider } from "@dashboardpack/core/i18n/locale-context";
+import { LocaleProvider } from "@dashboardpack/core/providers/locale-provider";
 import { Toaster } from "@dashboardpack/core/components/ui/sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
