8242c0cfdc03c2c6a451d79ed0bed57827e68314
Brendan Bartlett <brendan.bartlett@rilaunceston.com.au>
2026-05-24 21:31:32 +1000

    Add root page to show dashboard

--- src/components/dashboard/dashboard-shell.tsx
+++ src/components/dashboard/dashboard-shell.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import React from "react";
+import { Sidebar } from "./sidebar";
 
 export function DashboardShell({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen w-full bg-gray-50">
       
-      {/* Sidebar spacer */}
-      <div className="w-64 flex-shrink-0" />
+      {/* Real Sidebar */}
+      <div className="w-64 flex-shrink-0">
+        <Sidebar />
+      </div>
 
-      {/* Main content */}
-      <div className="flex-1 p-6">
+      {/* Main Content */}
+      <div className="flex-1 overflow-auto p-6">
         {children}
       </div>
 
